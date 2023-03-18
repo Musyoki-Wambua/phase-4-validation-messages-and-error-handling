a59b1f158fd6651a73fe88a201be35ecf22d0267
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-18 13:56:15 +0300

    Completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+    CATEGORIES = ["Comedy", 'Drama', 'Animation', 'Mystery', 'Horror', 'Fantasy', 'Action', 'Documentary', 'Science Fiction']
+    validates :title, presence: true
+    validates :poster_url, presence: true
+    validates :year, numericality: {
+        grater_than_or_equal_to: 1888, 
+        less_than_or_equal_to: Date.today.year
+    }
+    validates :category, inclusion: {
+        in: CATEGORIES, 
+        message: "Must be one of: #{CATEGORIES.join(', ')}"
+    }
 end
